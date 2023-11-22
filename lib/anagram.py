84178b4a59839f6b65583021ae995443324762ec
Kanny <kanny.ghafour87@gmail.com>
2023-11-22 09:43:25 +0300

    lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def is_anagram(self, word1, word2):
+        return sorted(word1) == sorted(word2)
+
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if self.is_anagram(self.word, word) and self.word != word:
+                matches.append(word)
+        return matches
